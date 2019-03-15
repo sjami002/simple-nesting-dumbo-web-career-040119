4e69051f6ee1fba11832a08fb959ed48f9bbd389
Shoptorshi Jamil <sjami002@gmail.com>
2019-03-15 20:57:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,7 +59,8 @@ def dennis_ritchies_language
         }
      }
 
-		 programmer_hash[:dennis_ritchie][:languages]
+		 array = programmer_hash[:dennis_ritchie][:languages]
+		 array.join
 end
 
 def adding_matz
